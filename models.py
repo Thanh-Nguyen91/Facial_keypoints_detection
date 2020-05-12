3ce8680189f82292b3c6e2da000d5d548f4314a9
Thanh Nguyen <Thithanhnguyen@protonmail.com>
2020-05-12 09:28:15 +0200

    first model

--- models.py
+++ models.py
@@ -21,6 +21,15 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32,64,3)
+        self.conv3 = nn.Conv2d(64,128,3)
+        self.conv4 = nn.Conv2d(128,256,3)
+        self.conv5 = nn.Conv2d(256,512,1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.drop = nn.Dropout(0.2)
+        self.fc1 = nn.Linear(512*6*6,1000)
+        self.fc2 = nn.Linear(1000,500)
+        self.fc3 = nn.Linear(500,136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -32,6 +41,29 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        # Imput: grayscale images of size 224x224
+        # 1x224x224 => 32x220x220 => 32x110x110
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.drop(x)
+        # 32x110x110 => 64x108x108 => 64x54x54
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.drop(x)
+        # 64x54x54 => 128x52x52 => 128x26x26
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop(x)
+        # 128x26x26 => 256x24x24 => 256x12x12
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop(x)
+        # 256x12x12 => 512x12x12 => 512x6x6
+        x = self.pool(F.relu(self.conv5(x)))
+        x = self.drop(x)
+        # 512x6x6 => 1x(512*6*6)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        x = F.relu(self.fc2(x))
+        x = self.drop(x)
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
